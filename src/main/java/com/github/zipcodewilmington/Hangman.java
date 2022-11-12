ad23b1072a822482a732beb87139a0c562cec073
AndrewAscone <andrew.ascone@gmail.com>
2022-11-12 15:24:23 -0500

    Continuing to fill out methods

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,6 +1,7 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,26 +22,41 @@ public class Hangman {
 
     String hiddenWord = "";
 
+    char[] gameWord;
+
+    ArrayList<Character> usedGuess = new ArrayList<Character>();
+
     public Hangman(){
 
     }
 
     public static void main(String[] args){
         Hangman game = new Hangman();
+
         game.runGame();
 
     }
 
     public void runGame(){
         while(gameFlag){
-
-            initialize_game_state();
+            announce_game();
             wordFlag = false;
 
+            initialize_game_state();
+
+
             while(!wordFlag){
+                print_current_state();
                 char nextGuess = get_next_guess();
+                usedGuess.add(nextGuess);
+                process(nextGuess);
 
             }
+
+            if(ask_to_play_again()){
+                continue;
+            }
+            break;
         }
         game_over();
 
@@ -60,12 +76,15 @@ public class Hangman {
         //String[] fourLetters = {};
         int randomPick = rand.nextInt(5) + 1;
         this.hiddenWord = threeLetters[randomPick];
+        this.gameWord = hiddenWord.toCharArray();
+        //this.gameWord = ['_','_','_'];
         this.numGuess = hiddenWord.length();
     }
 
     public Character get_next_guess(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Guess a letter!");
+        numGuess--;
         return scanner.next().charAt(0);
     }
 
@@ -79,13 +98,18 @@ public class Hangman {
     }
 
     public void print_current_state(){
-        System.out.println("Current Guesses: ");
+        System.out.println("Current Guesses: " + usedGuess);
 
         System.out.println("You have " + numGuess + " tries left");
     }
 
-    public void process(){
+    public void process(char guess){
+        for(int i = 0; i < gameWord.length; i++){
+            if(guess == gameWord[i]){
 
+
+            }
+        }
     }
 
     public String player_won(){
