a7376e902aceac0a6b313da80f5909d2dc92c234
AndrewAscone <andrew.ascone@gmail.com>
2022-11-28 13:26:57 -0500

    Word gues game running

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -24,6 +24,8 @@ public class Hangman {
 
     char[] gameWord;
 
+    char[] gameWordDisplay;
+
     ArrayList<Character> usedGuess = new ArrayList<Character>();
 
     public Hangman(){
@@ -39,27 +41,42 @@ public class Hangman {
 
     public void runGame(){
         while(gameFlag){
-            announce_game();
+            System.out.println(announce_game());
             wordFlag = false;
+            usedGuess.clear();
 
             initialize_game_state();
 
+            System.out.println("You have " + numGuess + " tries left");
+
+            displayGameWordState();
+
 
             while(!wordFlag){
-                print_current_state();
+
                 char nextGuess = get_next_guess();
                 usedGuess.add(nextGuess);
                 process(nextGuess);
+                wordFlag = is_word_guessed();
+                print_current_state();
+                if(numGuess == 0){
+                    break;
+                }
+            }
 
+            if(is_word_guessed()){
+                System.out.println(player_won());
+            }
+            else if(!is_word_guessed()){
+                System.out.println(player_lost());
             }
 
             if(ask_to_play_again()){
                 continue;
             }
+            System.out.println(game_over());
             break;
         }
-        game_over();
-
     }
 
     public String announce_game(){
@@ -72,28 +89,61 @@ public class Hangman {
 
     public void initialize_game_state(){
         Random rand = new Random();
-        String[] threeLetters = {"cat", "dog", "bat", "wit", "tin", "hat"};
-        //String[] fourLetters = {};
-        int randomPick = rand.nextInt(5) + 1;
-        this.hiddenWord = threeLetters[randomPick];
+        String[] words = {"cat", "dog", "bat", "farm", "miss", "time", "smash", "cross", "mouse"};
+
+        int randomPick = rand.nextInt(words.length) + 1;
+        hiddenWord = words[randomPick];
         this.gameWord = hiddenWord.toCharArray();
-        //this.gameWord = ['_','_','_'];
+        gameWordDisplay = new char[gameWord.length];
+        for (int i = 0; i < gameWordDisplay.length; i++) {
+            gameWordDisplay[i] = '_';
+        }
         this.numGuess = hiddenWord.length();
     }
 
     public Character get_next_guess(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Guess a letter!");
-        numGuess--;
-        return scanner.next().charAt(0);
+        char playerLetter = scanner.next().charAt(0);
+//
+//        if(!usedGuess.contains(playerLetter)){
+//            return playerLetter;
+//        }
+//        else{
+//            System.out.println("You've already guessed that letter. Guess another letter");
+//            scanner.next();
+//            get_next_guess();
+//
+//        }
+
+        return playerLetter;
     }
 
     public boolean is_word_guessed(){
-        return false;
+        for (int i = 0; i < gameWordDisplay.length; i++) {
+            if(gameWordDisplay[i] == '_'){
+                return false;
+            }
+        }
+        return true;
     }
 
     public boolean ask_to_play_again(){
-        System.out.println("Play again?");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Play again? (y/n)");
+        String playAgainChoice = scanner.nextLine();
+
+        if(playAgainChoice.equals("y")){
+            return true;
+        }
+        else if(playAgainChoice.equals("n")){
+            return false;
+        }
+        else{
+            System.out.println("Please make a valid selection");
+            ask_to_play_again();
+        }
+
         return false;
     }
 
@@ -101,13 +151,44 @@ public class Hangman {
         System.out.println("Current Guesses: " + usedGuess);
 
         System.out.println("You have " + numGuess + " tries left");
+
+        displayGameWordState();
     }
 
+    public void displayGameWordState(){
+        System.out.println("Word to guess: ");
+
+        for(char c : gameWordDisplay){
+            System.out.print(c + " ");
+        }
+        System.out.println("");
+    }
+
+
     public void process(char guess){
+        if(letterMatched(guess)){
+            saveCharacter(guess);
+        }
+        else{
+            System.out.println("That letter is not in the hidden word");
+            numGuess--;
+        }
+
+    }
+
+    private boolean letterMatched(char guess) {
         for(int i = 0; i < gameWord.length; i++){
             if(guess == gameWord[i]){
+                return true;
+            }
+        }
+        return false;
+    }
 
-
+    public void saveCharacter(char guess){
+        for (int i = 0; i < gameWord.length; i++) {
+            if(guess == gameWord[i]){
+                gameWordDisplay[i] = gameWord[i];
             }
         }
     }
